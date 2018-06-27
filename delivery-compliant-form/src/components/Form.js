7d87ef85a8f3828478dc4f217850267d43d15e84
Courtney Adams <cadams8238@gmail.com>
2018-06-27 11:52:36 -0700

    added validators and Input component

--- delivery-compliant-form/src/components/Form.js
+++ delivery-compliant-form/src/components/Form.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import {reduxForm, Field} from 'redux-form';
-import {required} from '../validators';
+import {required, notEmpty, isFiveCharLong, isNumber} from '../validators';
+import Input from './Input';
 
 export class DeliveryForm extends React.Component {
+
+  onSubmit(values) {
+    console.log(values);
+  }
 
   render() {
     return (
       <div>
         <h1>Report a problem with your delivery</h1>
-        <form>
+        <form
+          onSubmit={this.props.handleSubmit(values => 
+            this.onSubmit(values)
+          )}
+        >
 
           <label htmlFor="tracking-number">Tracking Number</label>
           <Field 
             name="tracking-number"
             id="tracking-number"
-            type="number"
-            component="input"
-            validate={[required]}
+            type="text"
+            component={Input}
+            validate={[required, notEmpty, isFiveCharLong, isNumber]}
           />
 
           <label htmlFor="issue">What is your issue?</label>
-          <Field name="issue" id="issue" component="select">
-            <option value="My delivery hasn't arrived">My delivery hasn't arrived</option>
-            <option value="The wrong item was delivered">The wrong item was delivered</option>
-            <option value="Part of my order was missing">Part of my order was missing</option>
-            <option value="Some of my order arrived damaged">Some of my order arrived damaged</option>
-            <option value="Other (Give more details below)">Other (Give more details below)</option>
+          <Field 
+            name="issue" 
+            id="issue" 
+            component={Input}
+          >
+              <option value="My delivery hasn't arrived">My delivery hasn't arrived</option>
+              <option value="The wrong item was delivered">The wrong item was delivered</option>
+              <option value="Part of my order was missing">Part of my order was missing</option>
+              <option value="Some of my order arrived damaged">Some of my order arrived damaged</option>
+              <option value="Other (Give more details below)">Other (Give more details below)</option>
           </Field>
           
           <label htmlFor="details">Give more details (optional)</label>
@@ -33,10 +46,13 @@ export class DeliveryForm extends React.Component {
             name="details"
             id="details"
             type="text"
-            component="textarea"
+            component={Input}
+            element="textarea"
           />
 
-          <button type="submit">Submit</button>
+          <button type="submit">
+            Submit
+          </button>
 
         </form>
       </div>
--- delivery-compliant-form/src/validators.js
+++ delivery-compliant-form/src/validators.js
@@ -1,1 +1,7 @@
 export const required = value => value ? undefined : 'Required';
+
+export const notEmpty = value => '' ? 'Must not be empty': undefined;
+
+export const isFiveCharLong = value => value.length === 5 ? undefined : 'Must be a 5 digit number';
+
+export const isNumber = value => (typeof value) === 'number' ? undefined : 'Must be a number';
